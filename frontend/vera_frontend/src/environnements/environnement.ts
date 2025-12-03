82d13a6f416977d405c9ac5f9315ce3d6eefc16d
MohamedEleisawy <eleisawy19@gmail.com>
2025-12-03 20:04:01 +0100

    url backend

--- frontend/vera_frontend/src/environnements/environnement.ts
+++ frontend/vera_frontend/src/environnements/environnement.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // ⚠️ Mets ici la VRAIE URL de ton backend Railway (sans le slash à la fin)
-  apiUrl: 'https://vera-backend.up.railway.app' 
+  apiUrl: 'https://vera-project-production.up.railway.app/' 
 };
