1e9cab75363d176a30aa1d4d5794a0ef1bea35e3
MohamedEleisawy <eleisawy19@gmail.com>
2025-12-04 23:29:56 +0100

    sondage

--- frontend/vera_frontend/src/app/features/dashboard/pages/dashboard-sondage/dashboard-sondage.ts
+++ frontend/vera_frontend/src/app/features/dashboard/pages/dashboard-sondage/dashboard-sondage.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-dashboard-sondage',
   templateUrl: './dashboard-sondage.html',
   styleUrls: ['./dashboard-sondage.css'],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class DashboardSondageComponent implements OnInit {
   sondageUrl: SafeResourceUrl;
